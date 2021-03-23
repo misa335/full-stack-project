26a2d907ddfc69343004ceef272b2d685f52f482
Misako Kondoh <misapipipink@gmail.com>
2021-03-24 04:18:31 +0900

    Fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -28,21 +28,17 @@ module.exports = {
         },
     },
 
-    // production: {
-    //     client: 'pg',
-    //     connection: {
-    //         host: '127.0.0.1',
-            //     user: process.env.DB_USER,
-            //     password: process.env.DB_PW,
-            //     database: process.env.DB_NAME
-            // },
-    //     pool: {
-    //         min:2,
-    //         max:10
-    //     },
-    //     migrations: {
-    //         directory: __dirname + "/server/migrations",
-    //         tableName: 'song_list'
-    //     },
-    // },
+    production: {
+        client: 'pg',
+        connection:
+            pgconfig || `postgres://${process.env.DB_USER}@127.0.0.1:5432/favorites`,
+        pool: {
+            min:2,
+            max:10
+        },
+        migrations: {
+            directory: __dirname + "/server/migrations",
+            tableName: 'song_list'
+        },
+    },
 };
