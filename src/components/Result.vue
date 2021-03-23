d7347d6af5887f6c86b7b50dc4fc0011f11743e4
Misako Kondoh <misapipipink@gmail.com>
2021-03-23 15:07:30 +0900

    Fixed Result.vue

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,12 +1,21 @@
 <template>
-    <!-- <img class="image" v-for="(photo, index) in photos" v-bind:key="index" v-bind:src="photo" @click="clickPhoto"/> -->
+<div>
+    <div class="result" v-for="(info, index) in infos" v-bind:key="index">
+        <a v-bind:href="info.trackViewUrl">{{info.trackName}}</a>
+        <br>
+        <a v-bind:href="info.previewUrl"><img class="image" v-bind:src="info.artworkUrl100"/></a>
+    </div>
+</div>
 </template>
 
 <script>
 
-// export default {
-//     name: 'Result'
-// }
+export default {
+    name: 'Result',
+    props: [
+       "infos" 
+    ]
+}
 </script>
 
 <style scoped>
