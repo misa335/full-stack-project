24276ee67d219cbec977400375663f3625c6fdd0
Misako Kondoh <misapipipink@gmail.com>
2021-03-24 14:04:00 +0900

    fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -13,9 +13,9 @@ module.exports = {
         connection: process.env.DATABASE_URL || {
             host: process.env.DATABASE_HOST || "127.0.0.1",
             port: process.env.DATABASE_PORT || "5432",
-            database: process.env.DATABASE_NAME || "favorite",
-            user: process.env.DATABASE_USER || "postgres",
-            password: process.env.DATABASE_PASSWORD || "kikilala",
+            database: process.env.DATABASE_NAME || process.env.DB_NAME,
+            user: process.env.DATABASE_USER || process.env.DB_USER,
+            password: process.env.DATABASE_PASSWORD || process.env.DB_PW,
           },
         // pool: {
         //     min:2,
