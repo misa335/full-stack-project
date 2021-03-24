1a90bd27f2a4be7b931f5cdb73182591a6af8be2
Misako Kondoh <misapipipink@gmail.com>
2021-03-24 13:52:10 +0900

    fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
     // development: {
         client: 'pg',
         connection: process.env.DATABASE_URL || {
-            host: process.env.DATABASE_HOST || "localhost:4000",
+            host: process.env.DATABASE_HOST || "127.0.0.1",
             port: process.env.DATABASE_PORT || "5432",
             database: process.env.DATABASE_NAME || "favorite",
             user: process.env.DATABASE_USER || "postgres",
